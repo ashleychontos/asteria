f7f83ca9fe1d720bcebcfc79dc4f8717e77280e1
Ashley Chontos <ashleychontos@gmail.com>
2022-03-18 16:59:22 -0400

    updated setup

--- setup.py
+++ setup.py
@@ -15,17 +15,17 @@ for line in open('requirements.txt', 'r').readlines():
 
 setuptools.setup(
     name="prot",
-    version=get_property('__version__', 'rotation'),
+    version=get_property('__version__', 'rotate'),
     license="MIT",
     author="Ashley Chontos",
     author_email="achontos@hawaii.edu",
     description="A quick package for estimating stellar rotation periods given their teff and logg",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/ashleychontos/rotation",
+    url="https://github.com/ashleychontos/rotate",
     project_urls={
-        "Source": "https://github.com/ashleychontos/rotation",
-        "Bug Tracker": "https://github.com/ashleychontos/rotation/issues",
+        "Source": "https://github.com/ashleychontos/rotate",
+        "Bug Tracker": "https://github.com/ashleychontos/rotate/issues",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -34,6 +34,6 @@ setuptools.setup(
     ],
     install_requires=reqs,
     packages=setuptools.find_packages(),
-    entry_points={'console_scripts':['rot=rotation.cli:main']},
+    entry_points={'console_scripts':['rotate=rotate.cli:main']},
     python_requires=">=3.6",
 )
